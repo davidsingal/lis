796ba2eaf9ad34b6d5fe5ff03fb04ff80e0da8c1
David Inga <david.inga@vizzuality.com>
2020-02-25 10:51:45 +0100

    Updates

---
+++ precache-manifest.9fb791eb3e4d852733cc10637151c3c8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12e37d50d9f88ae39c8def4de69d6873",
+    "url": "/index.html"
+  },
+  {
+    "revision": "087947ab777973a9250c",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5fd657d32489aaf76a98",
+    "url": "/static/js/2.9f1d5a3f.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.9f1d5a3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "087947ab777973a9250c",
+    "url": "/static/js/main.a1e3b2a9.chunk.js"
+  },
+  {
+    "revision": "eab6ea32f7b5f52ac83c",
+    "url": "/static/js/runtime-main.4d4038e0.js"
+  },
+  {
+    "revision": "59704167f3327f80ed02a1c235f2f785",
+    "url": "/static/media/2_no_clouds_8k.59704167.jpg"
+  },
+  {
+    "revision": "f2f9164023e76d554133f4e27ac507d6",
+    "url": "/static/media/elev_bump_4k.f2f91640.jpg"
+  },
+  {
+    "revision": "1c46fa10182c00d019cabcecfab7aaf0",
+    "url": "/static/media/galaxy_starfield_4k.1c46fa10.png"
+  },
+  {
+    "revision": "44670f840dfe730cddfcc24d79695e12",
+    "url": "/static/media/water_4k.44670f84.png"
+  }
+]);
